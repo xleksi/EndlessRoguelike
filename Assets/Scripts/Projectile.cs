4abf91b6174363c207c29988b78c627dddec6e89
leksi <skrip3151@gmail.com>
2024-03-17 14:44:43 +0100

    Added Projectile Destroy Time

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,7 +7,8 @@ public class Projectile : MonoBehaviour
     private Vector3 mousePos;
     private Camera mainCam;
     private Rigidbody2D rb;
-    public float speed;
+    public float Speed;
+    public float DestroyDelay;
     
     // Start is called before the first frame update
     void Start()
@@ -17,9 +18,10 @@ public class Projectile : MonoBehaviour
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
         Vector3 direction = mousePos - transform.position;
         Vector3 rotation = transform.position - mousePos;
-        rb.velocity = new Vector2(direction.x, direction.y).normalized * speed;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * Speed;
         float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, rot);
+        Destroy(gameObject, DestroyDelay);
     }
 
     // Update is called once per frame
